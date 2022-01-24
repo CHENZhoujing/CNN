6aae896369e7b1c81e1046d85a5fffda5e6a2eea
CHENZhoujing <zhoujingchen1@gmail.com>
2022-01-24 22:59:43 +0100

    Convolution version 1

--- Convolution.py
+++ Convolution.py
@@ -1,15 +1,22 @@
-import numpy
 import numpy as np
 
 
-class Conv3:
+class Conv:
+    def __init__(self, size, num_filters):
+        self.size = size #3,5,7
+        self.num_filters = num_filters
+        self.filters = np.random.randn(num_filters, size, size)/9 # Xavier,MSRA
+
     def iterate_regions(self, image):
         h, w = image.shape
-
-        for i in range(h - 2):
-            for j in range(w - 2):
-                im_region = image[i: (i + 3), j : (j+ 3)]
+        for i in range(h - self.size + 1):
+            for j in range(w - self.size + 1):
+                im_region = image[i: (i + self.size), j : (j+ self.size)]
+                yield im_region, i, j
 
     def forward(self, input):
         h, w = input.shape
-        output = np.zeros((h - 2))
+        output = np.zeros((h - self.size + 1, w - self.size + 1, self.num_filters))
+        for im_region, i, j in self.iterate_regions(input):
+            output[i, j] = np.sum(im_region * self.filters, axis=(1, 2))
+        return self.output
